1c9746cab170bd06a5d3c62738b7a22ea47a35c8
League Student <student@jointheleague.org>
2025-10-07 15:17:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/30_Turtle_Tricks/20_Turtle_Tricks.py
+++ lessons/10_Turtles/30_Turtle_Tricks/20_Turtle_Tricks.py
@@ -12,25 +12,24 @@ turtle.setup(600,600,0,0)               # Set the size of the window
 tina = turtle.Turtle()                  # Create a turtle named tina
 
 tina.pendown()
-tina.pencolr("red")
-tina.foward(90)
+tina.pencolor("red")
+tina.forward(90)
 tina.left(105)
 tina.pencolor("blue")
-tina.foward(90)
+tina.forward(90)
 tina.left(105)
 tina.pencolor("green")
-tina.foward(90)
+tina.forward(90)
 tina.left(105)
 tina.pencolor("pink")
-tina.foward(90)
+tina.forward(90)
 tina.left(105)
 tina.pencolor("orange")
-tina.foward(90)
+tina.forward(90)
 tina.left(105)
 # Use tina.forward() and tina.left() to draw a pentagon
 # Make each side of the pentagon a different color with 
 # tina.pencolor()
-
-... # Your code here
+ # Your code here
 
 turtle.exitonclick()                    # Close the window when we click on it
