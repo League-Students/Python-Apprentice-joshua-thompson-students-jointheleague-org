7266833e192f0bc081fbe58ee22efe60908114df
League Student <student@jointheleague.org>
2025-10-07 15:23:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/30_Turtle_Tricks/30_Turtle_Tricks.py
+++ lessons/10_Turtles/30_Turtle_Tricks/30_Turtle_Tricks.py
@@ -16,8 +16,12 @@ tina = turtle.Turtle()                  # Create a turtle named tina
 
 # Use tina.circle() to draw a circle, and tina.goto() to move tina to a new location
 # Use tina.begin_fill(), tina.end_fill(), and tina.fillcolor() to fill in the shapes
-
-tina.circle(90) # Your code here
+tina.pendown()
+tina.pencolor("red")
+tina.circle(90)
+tina.penup()
+tina.forward()
+ # Your code here
 
 turtle.exitonclick()                    # Close the window when we click on it
 
